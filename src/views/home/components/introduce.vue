716e979bdf759cb7b2fac9f0662fb3f608628103
vunguyen109 <80802912+vunguyen109@users.noreply.github.com>
2022-10-30 16:01:18 +0700

    Vu commit

--- src/views/home/components/introduce.vue
+++ src/views/home/components/introduce.vue
@@ -1,6 +1,9 @@
 <template>
   <section class="introduce">
-	introduce
+	<h1>Giới thiệu</h1>
+  <div class="text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis eos quos cum ipsam deserunt accusantium consequuntur fugiat accusamus illum fuga quisquam, adipisci, eligendi magnam delectus blanditiis porro labore reprehenderit? Ea facere numquam eaque porro doloremque modi recusandae quis repellat dolores sequi dicta corporis, inventore eum animi, saepe atque quia ut aliquid veritatis, provident cum possimus.
+
+  </div>
   </section>
 </template>
 
@@ -25,5 +28,9 @@ export default {
 	height: 50vh;
 	background-color: #ffffff;
 
+}
+.text {
+  color: black;
+  font-size: 18px;
 }
 </style>
--- src/views/home/components/map.vue
+++ src/views/home/components/map.vue
@@ -1,89 +1,65 @@
 <template>
-  <div class="map">
-	map
-    <!-- <div>
-      <label>
-        <gmap-autocomplete
-          @place_changed="setPlace">
-        </gmap-autocomplete> -->
-        <!-- <button @click="addMarker">Add</button> -->
-      <!-- </label>
-      <br/>
-
-    </div>
-    <br>
-    <gmap-map
-      :center="center"
-      :zoom="12"
-      style="width:100%;  height: 400px;"
-    >
-      <gmap-marker
-        :key="index"
-        v-for="(m, index) in markers"
-        :position="m.position"
-        @click="center=m.position"
-      ></gmap-marker>
-    </gmap-map> -->
+  <div class="map-wrap">
+    <a href="https://www.maptiler.com" class="watermark"><img
+        src="https://api.maptiler.com/resources/logo.svg" alt="MapTiler logo"/></a>
+    <div class="map" ref="mapContainer"></div>
   </div>
 </template>
 
 <script>
+import { Map } from 'maplibre-gl';
+import { shallowRef, onMounted, onUnmounted, markRaw } from 'vue';
+
 export default {
-//   name: "GoogleMap",
-//   data() {
-//     return {
-//         //mặc định là Montreal
-//       center: { lat: 45.508, lng: -73.587 },
-//       markers: [],
-//       places: [],
-//       currentPlace: null
-//     };
-//   },
+  name: "Map",
+  setup () {
+    const mapContainer = shallowRef(null);
+    const map = shallowRef(null);
 
-//   mounted() {
-//     this.geolocate();
-//   },
+    onMounted(() => {
+      const apiKey = 'GEnm2e5tMf4qwCZtnzmv';
+      //https://cloud.maptiler.com/account/keys
+      const initialState = { lng: 139.753, lat: 35.6844, zoom: 14 };
 
-//   methods: {
-//     // nhận địa điểm thông qua autocomplete component
-//     setPlace(place) {
-//       this.currentPlace = place;
-//     },
-//     addMarker() {
-//       if (this.currentPlace) {
-//         const marker = {
-//           lat: this.currentPlace.geometry.location.lat(),
-//           lng: this.currentPlace.geometry.location.lng()
-//         };
-//         this.markers.push({ position: marker });
-//         this.places.push(this.currentPlace);
-//         this.center = marker;
-//         this.currentPlace = null;
-//       }
-//     },
-//     geolocate: function() {
-//       navigator.geolocation.getCurrentPosition(position => {
-//         this.center = {
-//           lat: position.coords.latitude,
-//           lng: position.coords.longitude
-//         };
-//       });
-//     }
-//   }
+      map.value = markRaw(new Map({
+        container: mapContainer.value,
+        style: `https://api.maptiler.com/maps/streets/style.json?key=${apiKey}`,
+        center: [initialState.lng, initialState.lat],
+        zoom: initialState.zoom
+      }));
+
+    }),
+    onUnmounted(() => {
+      map.value?.remove();
+    })
+
+    return {
+      map, mapContainer
+    };
+  }
 };
 </script>
-<style lang="scss" scoped>
-.map {
-	height: 100vh;
-	background-color: #00B661;
+
+
+<style scoped>
+@import '~maplibre-gl/dist/maplibre-gl.css';
+
+.map-wrap {
+  position: relative;
+  width: 100%;
+  height: calc(100vh - 77px); /* calculate height of the screen minus the heading */
 }
-.pac-target-input {
-	position: absolute;
-    width: 80%;
-    left: 10%;
-    top: 157px;
-    z-index: 1;
-    border: 2px solid lightgray;
-    border-radius: 5px;
+
+.map {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+}
+
+.watermark {
+  position: absolute;
+  left: 10px;
+  bottom: 10px;
+  z-index: 999;
 }
 </style>
