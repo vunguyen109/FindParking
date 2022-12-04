e112160c2e2dc289db89846ae206b4dbe3af9c14
vunguyen109 <80802912+vunguyen109@users.noreply.github.com>
2022-12-04 09:42:59 +0700

    Vu commit

--- FE/src/views/userDetail.vue
+++ FE/src/views/userDetail.vue
@@ -8,6 +8,27 @@
         <input type="text" v-model="userDetail.phone">
         <button @click="update">cập nhật</button>
         </div>
+        <div class="">
+        <table class="table">
+          <thead slot="head">
+            <th>tên bãi đỗ xe</th>
+            <th>biển số xe</th>
+            <th>thời gian đặt</th>
+            <th>giá tiền</th>
+            <th>trang thái</th>
+          </thead>
+          <tbody >
+            <tr v-for="row in order_history" :key="row.orderId" >
+              <td>{{ row.userName }}</td>
+              <td>{{ row.licensePlate }}</td>
+              <td>{{ row.time }}</td>
+              <td>{{ row.price }}</td>
+              <td v-if="(row.status == 0)">chưa đến</td>
+              <td v-else>{{ row.price }}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
 </template>
 
@@ -17,6 +38,7 @@ export default {
   data() {
     return {
       userDetail: {},
+      order_history: {},
     };
   },
   components: {
@@ -26,8 +48,11 @@ export default {
   },
   methods: {
     init() {
-      this.$api.get("users/"+localStorage.getItem("userId")).then((response) => {
-        this.userDetail = response;
+      this.$api.get("users/"+localStorage.getItem("userId")).then((response1) => {
+        this.$api.get("users/orderHistory/"+localStorage.getItem("userId")).then((response2) => {
+        this.order_history = response2;
+      });
+        this.userDetail = response1;
       });
     },
     switchVisibility() {
